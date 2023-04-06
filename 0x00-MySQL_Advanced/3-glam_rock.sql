58330bffe2f2c63bc3d327394b0a8a5f0aed48b4
AbdiAbader <abdiprocode@gmail.com>
2023-04-06 15:47:39 +0300

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- galm 
-SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+-- SElet 
+SELECT band_name, (IFNULL(split, 2022)-formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC
+WHERE style LIKE '%Glam Rock%'
+ORDER BY lifespan DESC;
