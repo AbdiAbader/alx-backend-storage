52b36afcb48951a1453a2c30ec9ef40c07fc9f31
AbdiAbader <abdiprocode@gmail.com>
2023-04-06 10:36:10 +0300

    fix first line comment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- create a unique user table
 CREATE TABLE IF NOT EXISTS users {
     id INTEGER NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
+    name varchar(255) ,
+    email varchar(255) NOT NULL unique,
+    primary key (id)
 }
