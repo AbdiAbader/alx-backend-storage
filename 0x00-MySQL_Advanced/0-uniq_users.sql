9c33adba2b4899039da749eb628412616c53b5eb
AbdiAbader <abdiprocode@gmail.com>
2023-04-06 10:27:07 +0300

    reomve comma

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- create a unique user table
 CREATE TABLE IF NOT EXISTS user {
-    id int NOT NULL AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
+    email varchar(255) NOT NULL
 }
