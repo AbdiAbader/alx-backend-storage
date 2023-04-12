63b657dc608b39de2fdd73b094dc5733e8662500
AbdiAbader <abdiprocode@gmail.com>
2023-04-12 15:16:01 +0300

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,19 +1,30 @@
-import requests
 import redis
+import requests
+from functools import wraps
 
 redis_client = redis.Redis()
 
+def cache_decorator(func):
+    @wraps(func)
+    def wrapper(url):
+        count_key = f"count:{url}"
+        html = redis_client.get(url)
+
+        if html is None:
+            response = requests.get(url)
+            html = response.text
+            redis_client.setex(url, 10, html)
+        else:
+            redis_client.incr(count_key)
+
+        redis_client.expire(count_key, 10)
+
+        return html
+
+    return wrapper
+
+@cache_decorator
 def get_page(url: str) -> str:
-    count_key = f"count:{url}"
-    html = redis_client.get(url)
+    response = requests.get(url)
+    return response.text
 
-    if html is None:
-        response = requests.get(url)
-        html = response.text
-        redis_client.setex(url, 10, html)
-    else:
-        redis_client.incr(count_key)
-
-    redis_client.expire(count_key, 10)
-
-    return html
